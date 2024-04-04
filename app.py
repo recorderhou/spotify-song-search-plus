d06c49cfe207749d7206026dd707d0f8489398c8
recorderhou <1900012987@pku.edu.cn>
2024-04-04 02:02:36 -0700

    data manager functions js/html

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from db import client
 import os
@@ -7,9 +8,11 @@ app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
 
+
 @app.route('/')
 def login():
     return render_template('login.html')
+
 
 @app.route('/admin')
 def admin():
@@ -42,25 +45,72 @@ def user_auth():
     password = user_login['password']
     print(username, password)
     print(username)
-    user = client.distdb0.user.find_one({'username': username, 'password': password})
+    user = client.distdb0.user.find_one({'username': username})
     if user:
-        print('correct')
-        session['username'] = username
-        return jsonify({'success': True, 'redirect_url': url_for('search')})
+        if user['password'] == password:
+            session['username'] = username
+            return jsonify({'success': True, 'redirect_url': url_for('search')})
+        else:
+            return jsonify({'success': False, 'redirect_url': url_for('wrong_auth')})
     else:
-        return jsonify({'success': False, 'redirect_url': url_for('wrong_auth')})
+        client.distdb0.user.insert_one({'username': username, 'password': password})
+
+
+@app.route('/session_data')
+def get_session_data():
+    print(session)
+    if 'username' in session:
+        return jsonify({'username': session['username'], 'admin_name': None})
+    elif 'admin_name' in session:
+        return jsonify({'username': None, 'admin_name': session['admin_name']})
+    return jsonify({'username': None, 'admin_name': None})
+
+
+@app.route('/rating', methods=['POST', 'GET'])
+def get_rating():
+    if request.method == 'GET':
+        cur_user = client.distdb0.user.find_one({'username': session['username']})
+        print('user in get', cur_user['rating'])
+        if 'rating' in cur_user:
+            return jsonify({'rating': cur_user['rating']})
+        else:
+            return jsonify({'rating': []})
+    elif request.method == 'POST':
+        new_rating = request.json
+        cur_user = client.distdb0.user.find_one({'username': session['username']})
+        print('find user', cur_user)
+        print('new rating', new_rating)
+        if 'rating' in cur_user:
+            cur_rating = cur_user['rating']
+            past_songs = [str(rate['song_id']) for rate in cur_rating]
+            if new_rating['song_id'] in past_songs:
+                index = past_songs.index(new_rating['song_id'])
+                cur_rating[index]['rating'] = new_rating['rating']
+            else:
+                cur_rating.append(new_rating)
+        else:
+            cur_rating = [new_rating]
+        try:
+            client.distdb0.user.update_one({'username': session['username']}, {'$set': {'rating': cur_rating}}, upsert=True)
+            return jsonify({'success': True})
+        except:
+            return jsonify({'success': False})
+
 
 @app.route('/modify')
 def modify():
     return render_template('modify.html')
 
+
 @app.route('/wrongauth')
 def wrong_auth():
     return render_template('wrong_auth.html')
 
+
 @app.route('/search')
 def search():
     return render_template('search_result.html')
+
 
 @app.route('/searchresult')
 def query():
@@ -77,6 +127,35 @@ def query():
             print(document)
             res.append(document)
     return jsonify(res)
+
+
+@app.route('/adminquery')
+def delete_query():
+    song = request.args.get('song', default='', type=str)
+    artist = request.args.get('artist', default='', type=str)
+    collection = client.distdb0.test_delete
+    res = []
+    query = {'name': {'$regex': song, '$options': 'i'}, 'prim_artist': {'$regex': artist, '$options': 'i'} }
+    for document in collection.find(query):
+        document['_id'] = str(document['_id'])
+        for video in document['video_info']:
+            video['_id'] = str(video['_id'])
+        res.append(document)
+    return jsonify(res)
+
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete():
+    deletion = request.json
+    print(deletion)
+    try:
+        deletion_id = ObjectId(deletion['_id'])
+        client.distdb0.test_delete.delete_one({'_id': deletion_id})
+        return ({'success': True})
+    except:
+        return ({'success': False})
+
+
 
 
 if __name__ == '__main__':
--- static/search.js
+++ static/search.js
@@ -1,7 +1,10 @@
 let inputQuery;
+let currentUser = '';
+let clicked = new Array(20).fill(false);
+let userRating = new Array(0);
 
 function submitQuery(){
-    query = `/searchresult?query=${encodeURIComponent(inputQuery)}`
+    let query = `/searchresult?query=${encodeURIComponent(inputQuery)}`
     return fetch(query)
         .then(response => response.json())
         .then(data => {
@@ -10,12 +13,102 @@ function submitQuery(){
         });
 }
 
+function getRating(){
+    let query = `/rating?query=${encodeURIComponent(currentUser)}`
+    return fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            return data
+        });
+}
+
+function updateStars(rate_div, curRating, resIndex, res) {
+    let stars = rate_div.querySelectorAll('.rating-star');
+            console.log(stars.length)
+            console.log('currently in', this.tagName);
+            // Add click event listeners to each star
+            stars.forEach(star => {
+                let rating = Array.from(stars).indexOf(star);
+                if (rating < curRating) {
+                    star.style.fill = 'goldenrod';
+                }
+            });
+
+            stars.forEach(star => {
+                let rating = Array.from(stars).indexOf(star);
+                if (rating < curRating) {
+                    star.style.fill = 'goldenrod';
+                }
+                star.onclick = function (event) {
+                    event.stopPropagation()
+                    clicked[resIndex] = true;
+
+                    console.log('inside onclick')
+
+                    let ratingData = {
+                        'song_id': res['_id'],
+                        'rating': rating + 1
+                    }
+
+                    fetch('/rating', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(ratingData)
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.success) {
+                                console.log('rate successful');
+                            } else {
+                                console.log('rate failed');
+                            }
+                        })
+
+                    // Set the rating based on the star's data-value attribute
+                    console.log('click rating', rating)
+                    for (let i = 0; i < stars.length; i++) {
+                        if (i <= rating) {
+                            stars[i].style.fill = 'goldenrod';
+                        } else {
+                            stars[i].style.fill = 'grey';
+                        }
+                    }
+                    console.log(`Rated: ${rating} stars`);
+                };
+                star.addEventListener('mouseenter', () => {
+                    if (!clicked[resIndex]) {
+                        console.log('mouseenter rating', rating)
+                        for (let i = 0; i < stars.length; i++) {
+                            if (i <= rating) {
+                                stars[i].style.fill = 'goldenrod';
+                            } else {
+                                stars[i].style.fill = 'grey';
+                            }
+                        }
+                    }
+                });
+
+                star.addEventListener('mouseleave', () => {
+                    if (!clicked[resIndex]) {
+                        console.log('mouseenter rating', rating)
+                        if (rating === 0) {
+                            stars[0].style.fill = 'grey';
+                        }
+                    }
+                });
+            });
+}
+
+
 function displayResult() {
     submitQuery().then( result=> {
         console.log(result)
         let element = document.getElementById('search-placeholder')
         let displayedResult = 0
-        for (let res of result) {
+        for (let [resIndex, res] of result.entries()) {
             if (displayedResult === 20) {
                 break;
             }
@@ -40,8 +133,7 @@ function displayResult() {
                     <p class="result-video"><a href="${videoLink}" target="_blank">Video Link</a></p>
                 `
                 div.appendChild(p)
-            }
-            else {
+            } else {
                 let p = document.createElement('div')
                 p.className = 'video-link'
                 p.innerHTML = `
@@ -49,8 +141,60 @@ function displayResult() {
                 `
                 div.appendChild(p)
             }
+
+            let rate_div = document.createElement('div')
+            rate_div.className = 'rate-button'
+            let curRating = 0;
+            console.log('userRating inside click', userRating);
+            let targetIndex = userRating.map(obj => obj['song_id']).indexOf(res['_id']);
+            console.log('targetIndex', targetIndex)
+            if (targetIndex >= 0) {
+                curRating = userRating[targetIndex].rating;
+                clicked[resIndex] = true;
+                rate_div.innerHTML = `
+                    <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                    </svg>
+                    <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                    </svg>
+                    <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                    </svg>
+                    <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                    </svg>
+                    <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                    </svg>
+                `;
+                updateStars(rate_div, curRating, resIndex, res);
+            } else {
+                rate_div.innerHTML = `<p class="rate-init">Rate</p>`
+                rate_div.onclick = function () {
+                    this.innerHTML = `
+                        <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                        </svg>
+                        <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                        </svg>
+                        <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                        </svg>
+                        <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                        </svg>
+                        <svg class="starIcon rating-star" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                        </svg>
+                    `;
+                    updateStars(this, curRating, resIndex, res);
+                }
+            }
+            div.appendChild(rate_div)
             element.appendChild(div)
-            displayedResult ++;
+            displayedResult++;
         }
     })
 }
@@ -61,5 +205,24 @@ document.getElementById('query-input').addEventListener('submit', function (e) {
     element.innerHTML = ''
     inputQuery = document.getElementById('query-text').value
     console.log(inputQuery)
-    displayResult()
+    fetch('/session_data')
+        .then(result => result.json())
+        .then(data => {
+            if (data['username']) {
+                currentUser = data['username']
+                console.log('current User is', currentUser)
+                let ratingQuery = `/rating?query=${encodeURIComponent(currentUser)}`
+                fetch(ratingQuery)
+                    .then(response => response.json())
+                    .then(data => {
+                        userRating = data.rating
+                    });
+                console.log('userRating is', userRating);
+                displayResult()
+            }
+            else {
+                element.innerHTML = '<p id="no-login-warn">You need to login to view content! Go to <a href="/user">Login</a></p>'
+            }
+        }
+        )
 })
